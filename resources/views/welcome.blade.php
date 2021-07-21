864b811868b946e59552323359645cc284790c2f
Davey Mason <daveymason@outlook.com>
2021-07-22 00:08:40 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,8 +78,12 @@
       <h1>Welcome</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       <hr>
-      <h3>Test</h3>
-      <p>Lorem ipsum...</p>
+      <h3>To-Do</h3>
+      <ul>
+        <li>Remove Blade</li>
+        <li>Upgrade Bootstrap</li>
+        <li>Integrate React </li>
+      </ul>
     </div>
     <div class="col-sm-2 sidenav">
       <div class="well">
